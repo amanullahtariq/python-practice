db4319278eec833f32c9fc40e19882a3f3eead69
Amanullah Tariq <amanullah.tariq@neclab.eu>
2019-04-05 17:41:17 +0200

    files imported

---
+++ multiple_output_classification.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Lambda
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+import tensorflow as tf
+
+
