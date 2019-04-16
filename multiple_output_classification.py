9e6b51a44272476516a59235286e88a2d921d95d
Amanullah Tariq <amanullah.tariq@neclab.eu>
2019-04-16 08:29:41 +0200

    Model function completed

--- multiple_output_classification.py
+++ multiple_output_classification.py
@@ -72,7 +72,7 @@ class FashionNet:
         return x
 
     @staticmethod
-    def build_color_branch(inputs,numColor,finalAct="softmax",chanDim=1):
+    def build_color_branch(inputs,numColors,finalAct="softmax",chanDim=1):
         # CONV => RELU => POOL
         x = Conv2D(16,(3,3),padding="same")(inputs)
         x = Activation("relu")(x)
@@ -82,3 +82,47 @@ class FashionNet:
 
         # CONV => RELU => POOL
         x = Conv2D(32, (3,3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
+        x = MaxPooling2D(pool_size=(2,2))(x)
+        x = Dropout(0.25)(x)
+
+        # CONV => RELU => POOL
+        x = Conv2D(32,(3,3),padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(pool_size=(2,2))(x)
+        x = BatchNormalization(axis=chanDim)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+
+        # define a branch of output layers for the number of different
+        # colors (i.e. red, black, blue, etc.)
+        x = Flatten(x)
+        x = Dense(128)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.5)(x)
+        x = Dense(numColors)(x)
+        x = Activation(finalAct, name="color_output")(x)
+
+        # return the color prediction sub-network
+        return x
+
+    @staticmethod
+    def build_model(width, height, numCategories, numColors, finalAct="softmax"):
+
+        # intializing the input shape and channel dimension (this code assume you are
+        # using TensorFlow which utilizes channels las ordering)
+
+        inputShape = (height,width,3)
+        chanDims = -1
+
+        input = Input(shape=inputShape)
+
+        # construct both the "category" and "color" sub-networks
+        categoryBranch = FashionNet.build_category_branch(input, numCategories,finalAct="softmax",chanDim=chanDims)
+        colorBranch = FashionNet.build_color_branch(input, numColors,finalAct="softmax",chanDim=chanDims)
+
+        # create model combining category and color branch
+        model = Model(inputs= input, outputs=[categoryBranch,colorBranch], name="fashionnet")
+        return model
