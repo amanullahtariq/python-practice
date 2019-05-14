5f5a6365835fcc24c2f36de73e98ff4960319d24
Amanullah Tariq <nle5257@office.hd>
2019-05-14 11:31:11 +0200

    file added for bike sharing

--- multiple_loss/train.py
+++ multiple_loss/train.py
@@ -8,7 +8,7 @@ from keras.optimizers import Adam
 from keras.preprocessing.image import img_to_array
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
-from multiple_loss.multiple_output_classification import FashionNet
+from multiple_output_classification import FashionNet
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
